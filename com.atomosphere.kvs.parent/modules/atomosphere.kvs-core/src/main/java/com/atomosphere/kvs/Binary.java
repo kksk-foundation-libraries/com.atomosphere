d6ea568dabea7d536fb0f110c891aec6487d81fe
kakusuke-maruno <kakusuke.maruno@gmail.com>
2019-10-19 02:03:45 +0900

    とりあえずlombokもれ。

--- com.atomosphere.kvs.parent/modules/atomosphere.kvs-core/src/main/java/com/atomosphere/kvs/Binary.java
+++ com.atomosphere.kvs.parent/modules/atomosphere.kvs-core/src/main/java/com/atomosphere/kvs/Binary.java
@@ -1,34 +1,14 @@
 package com.atomosphere.kvs;
 
-import java.util.Arrays;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Binary {
-	public final byte[] data;
-
-	public Binary(byte[] data) {
-		this.data = data;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + Arrays.hashCode(data);
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Binary other = (Binary) obj;
-		if (!Arrays.equals(data, other.data))
-			return false;
-		return true;
-	}
-
+	public byte[] data;
 }
